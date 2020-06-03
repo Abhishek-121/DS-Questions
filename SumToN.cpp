7f12f5a665c9b582efc80b01e1bb1c1ea288c699
Abhishek Maheshwari <abhishek69744maheshwari@gmail.com>
2020-06-03 09:06:08 +0530

    First commit

---
+++ SumToN.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,target;
+    cin>>n>>target;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    for(int i=0;i<n-1;i++){
+        for(int j = i+1;j<n;j++){
+            if(arr[i]+arr[j]==target){
+                cout<<"Indices are :"<<i<<" and "<<j;
+                break;
+            }
+        }
+    }
+
+
+  return 0;
+}
